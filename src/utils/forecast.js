7ed9fa497e4c0b0543cdd6e8b95bde2de2201fda
Jirah Angela Prado <jirahangelaprado@gmail.com>
2021-08-29 20:34:39 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=c86d18afe5da3609f66d5871d02c4198&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degrees out. It feels like " +
+          body.current.feelslike +
+          " degrees out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
